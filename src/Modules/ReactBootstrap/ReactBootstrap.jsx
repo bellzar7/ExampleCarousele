708243404e09c93d2f0b93ffa84cc551dc133f25
Nazariy Belelya <belelya.nazar1999@gmail.com>
2024-07-30 22:03:42 +0300

    add links

--- src/Modules/ReactBootstrap/ReactBootstrap.jsx
+++ src/Modules/ReactBootstrap/ReactBootstrap.jsx
@@ -13,31 +13,32 @@ function ReactBootstrap() {
     return (
         <div className={'customContainer'}>
             <h1>Приклад 3 (Бібліотека ReactBootstrap)</h1>
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <img style={{width: '100%', height: '400px'}} src={results_leus} alt=""/>
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img style={{width: '100%', height: '400px'}} src={results_leus} alt=""/>
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img style={{width: '100%', height: '400px'}} src={results_leus} alt=""/>
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel></div>
+            <a href="https://react-bootstrap.netlify.app/docs/components/carousel" target={'_blank'}>Приклади лібки</a>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img style={{width: '100%', height: '400px'}} src={results_leus} alt=""/>
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img style={{width: '100%', height: '400px'}} src={results_leus} alt=""/>
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img style={{width: '100%', height: '400px'}} src={results_leus} alt=""/>
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel></div>
     );
 }
 
--- src/Modules/Results/Results.jsx
+++ src/Modules/Results/Results.jsx
@@ -30,6 +30,7 @@ const Results = () => {
           <h2 className={style.wrap_top__title}>
            Приклад 1 (Сайт Znaesh Agency)
           </h2>
+            <a href="https://www.figma.com/design/OKyvaS1E22pwup6EfkBq4s/%D0%97%D0%BD%D0%B0%D1%94%D1%88-%D0%A1%D0%B0%D0%B9%D1%82---Redesign?node-id=949-5936&m=dev" target={'_blank'}>Фігма</a>
         </div>
         <div className={style.wrap_bottom}>
           <Carousel data={items}/>
